f27cf4496a5d8012d5ee82b1a2f8290d9ae75767
schoeggel <schoeggel@gmx.ch>
2025-12-01 22:11:17 +0100

    aoc puzzle 1 Part 1

--- scr/1-thesafe/src/main.cpp
+++ scr/1-thesafe/src/main.cpp
@@ -1,5 +1,6 @@
 // https://adventofcode.com/2025/day/1
 
+#include <algorithm>
 #include <assert.h>
 #include <filesystem>
 #include <fstream>
@@ -8,17 +9,18 @@
 
 struct Safe {
   const int _numbers; // 0..99 --> 100 numbers
-  int _position{0};
+  int _position{50}; // dial starts pointing at 50
   int _zeroHits{0};
 
   Safe(int numbers) : _numbers(numbers) {}
 
   void Turn(int inc) {
-    inc = inc % _numbers;
-    if (inc < 0) {
-      inc = _numbers + inc;
-    }
     _position += inc;
+    _position = _position % _numbers;
+
+    if (_position < 0) {
+      _position = _numbers + _position;
+    }
 
     if (_position == 0) {
       _zeroHits++;
@@ -49,17 +51,25 @@ bool RunTests() {
     safe.Turn(-103);
     assert(safe._position == 7);
   }
-
   {
     Safe safe{100};
     safe.Turn(-103);
-    assert(safe._position == 97);
-    
+    assert(safe._position == 47);
     safe.Turn(+103);
+    assert(safe._position == 50);
+    assert(safe._zeroHits == 0);
+    safe.Turn(+50);
     assert(safe._position == 0);
     assert(safe._zeroHits == 1);
+    safe.Turn(-260);
+    assert(safe._position == 40);
+    safe.Turn(+60);
+    assert(safe._position == 0);
+    assert(safe._zeroHits == 2);
+    safe.Turn(+0);
+    assert(safe._position == 0);
+    assert(safe._zeroHits == 3);
   }
-
 
   return true;
 }
@@ -71,19 +81,25 @@ int main() {
   }
 
   std::cout << "Current path is " << std::filesystem::current_path() << '\n';
-
   std::string line;
-
   std::ifstream file("../../../../rotations.txt");
+
+  Safe safe{100};
 
   if (file.is_open()) {
     while (std::getline(file, line)) {
-      std::cout << line << std::endl;
+      std::replace(line.begin(), line.end(), 'L', '-');
+      std::replace(line.begin(), line.end(), 'R', '+');
+      auto inc = std::stoi(line);
+      std::cout << inc << std::endl;
+      safe.Turn(inc);
     }
     file.close();
   } else {
     std::cout << "Error: Unable to open file." << std::endl;
   }
+
+  std::cout << "Secret: " << safe.getHits() << "\n";
 
   return 0;
 }
